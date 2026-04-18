50ff40804d4d7f7cfe25122885161c0372b958b4
0xmennatullah <0xmenatullah@gmail.com>
2026-04-18 12:08:37 +0200

    vault backup: 2026-04-18 12:08:37

--- Routes/base.py
+++ Routes/base.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI, APIRouter
 
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix="/api/v1", #prefix for all the routes
+    tags=["api_v1"],
+)
+
+
 
 @base_router.get("/")
 def welcome():
     return{
-        "message": "hello cutie :>"
+        "message": "hello all :>" #can be changed n reloaded on the fly
 
     }
+
+
